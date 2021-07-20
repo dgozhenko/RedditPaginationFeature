ac4d3f3f47d8e78a22efb2ec0ec89f35445f74d2
daniil.gozhenko <daniil.gozhenko@gmail.com>
2021-07-20 15:46:03 +0300

    Refactor ui folders

--- app/src/main/java/com/example/redditpagination/ui/MainActivity.kt
+++ app/src/main/java/com/example/redditpagination/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.redditpagination
+package com.example.redditpagination.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.redditpagination.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
