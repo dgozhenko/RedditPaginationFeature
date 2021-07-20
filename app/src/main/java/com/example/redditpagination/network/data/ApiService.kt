dab3fba191469a415238dee716d96077cf53b50a
daniil.gozhenko <daniil.gozhenko@gmail.com>
2021-07-20 12:57:17 +0300

    Create API response + service

---
+++ app/src/main/java/com/example/redditpagination/network/data/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.redditpagination.network.data
+
+import com.example.redditpagination.network.response.RedditApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("/r/aww/hot.json")
+    suspend fun fetchPosts(
+        @Query("limit") loadSize: Int = 10,
+        @Query("after") after: String? = null,
+        @Query("before") before: String? = null
+    ): Response<RedditApiResponse>
+}
---
+++ app/src/main/java/com/example/redditpagination/network/response/RedditListing.kt
@@ -0,0 +1,3 @@
+package com.example.redditpagination.network.response
+
+class RedditListing (val children: List<PostContainer>, val after: String?, val before: String?)
